6b7a1be0d5bbbecc70ff5c83952d784c456199de
Maciej Manna <maciejmanna@gmail.com>
2026-02-17 04:38:34 +0100

    fix for pre-check stage

--- src/snowdrift/breeze_cpp/stub.py
+++ src/snowdrift/breeze_cpp/stub.py
@@ -6,9 +6,6 @@ def hello(x: int) -> str:
     return f'Hello from breeze (C++)! The answer is {x}!'
 
 
-xxxxx
-
-
 def answer() -> int:
     """Sample answer function to test that the module is working correctly."""
     return 42
